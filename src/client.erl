e67e90b64fe652fa1e3d8b2166751eb944aa8334
p-kino <>
2023-07-04 09:03:00 +0900

    クライアントの処理を途中まで実装した

--- src/client.erl
+++ src/client.erl
@@ -2,12 +2,35 @@
 -expoer().
 
 login(Name, Pid) ->
+    ClientPid = spawn(?MODULE, init_loop, [Name]),
+
+
     case Pid ! {login, self(), Name} of
         {ok, ChatHistory} ->
             lists:foreach(fun({N, C}) -> io:format("~s> ~s~n", [N, C]) end,
-                          ChatHistory),
-            % クライアントのプロセスをspawnして返す
+                          lists:reverse(ChatHistory)),
+            loop();
 
         used_name ->
-            io:format("User name ~"~s~" is already used.", [Name]).
+            io:format("User name \"~s\" is already used.", [Name])
+    end.
+
+init_loop() ->
+    Pid = whereis(chat_server),
+    monitor(process, Pid),
+    loop().
+
+loop() ->
+    receive
+        logout ->
+            Pid = whereis(chat_server),
+            Pid ! {logout, self()},
+            exit(normal);
+        {'DOWN', _, _, _, _} ->
+            io:format("Server downed."),
+            exit(normal);
+        Unknown ->
+            io:format("Unknown message: ~p~n", [Unknown]),
+            loop()
+    end.
 
